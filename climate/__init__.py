4b8344c2e8d960098e10c7807c776b9df1434a24
Zuotian Tatum <z.tatum@lumc.nl>
2012-10-09 15:40:22 +0200

    Copy static files from climate trunk. Set up basic view urls.

--- climate/__init__.py
+++ climate/__init__.py
@@ -4,9 +4,9 @@
 @contact: z.tatum@lumc.nl
 """
 
-
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_pyfile('../climate.cfg')
 
 from . import views
--- climate/views.py
+++ climate/views.py
@@ -3,9 +3,31 @@
 @author: Zuotian Tatum
 @contact: z.tatum@lumc.nl
 """
+from flask import render_template
 
 from climate import app
 
+view_context = {
+    'menu' : [
+        ['About Us', 'about'],
+        ['Interface Generation', 'generate'],
+        ['Tool Definition', 'define']],
+    'version' : app.config['VERSION'],
+    'debug_js' : app.config['DEBUG']
+}
+
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html', **view_context)
+
+@app.route('/define')
+def define():
+    pass
+
+@app.route('/generate')
+def generate():
+    pass
+
+@app.route('/about')
+def about():
+    return render_template('about.html', **view_context)
