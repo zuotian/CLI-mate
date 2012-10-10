21fe9aaa75aa42c665b880dd5cbdf86e15f31fa5
Zuotian Tatum <z.tatum@lumc.nl>
2012-10-10 10:55:22 +0200

    Use context_processor to manager custom context variables.

--- climate/views.py
+++ climate/views.py
@@ -8,34 +8,32 @@ from flask import render_template
 from climate import app
 from .forms import ToolForm, ToolUploadForm, ParameterForm
 
-view_context = {
-    'menu' : [
-        ['About Us', 'about'],
-        ['Interface Generation', 'generate'],
-        ['Tool Definition', 'define']],
-    'version' : app.config['VERSION'],
-    'debug_js' : app.config['DEBUG']
-}
+@app.context_processor
+def view_context():
+    return {
+        'menu': [['About Us', 'about'],
+                  ['Interface Generation', 'generate'],
+                  ['Tool Definition', 'define']]}
 
 @app.route('/')
 def index():
-    return render_template('index.html', **view_context)
+    return render_template('index.html')
 
 @app.route('/define/upload')
 def define_upload():
     upload_form = ToolUploadForm()
-    return render_template('tool/upload.html', upload_form=upload_form, **view_context)
+    return render_template('tool/upload.html', upload_form=upload_form)
 
 @app.route('/define')
 def define():
     form = ToolForm(csrf_enabled=True)
-    return render_template('define.html',  form=form, data={}, **view_context)
+    return render_template('define.html',  form=form, data={})
 
 @app.route('/define/new')
 def define_new():
     form = ToolForm(csrf_enabled=True)
     parameter_form = ParameterForm()
-    return render_template('tool/new.html', form=form, parameter_form=parameter_form, data={}, **view_context)
+    return render_template('tool/new.html', form=form, parameter_form=parameter_form, data={})
 
 @app.route('/generate')
 def generate():
@@ -43,4 +41,4 @@ def generate():
 
 @app.route('/about')
 def about():
-    return render_template('about.html', **view_context)
+    return render_template('about.html')
